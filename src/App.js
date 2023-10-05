dd50533f4a2cfcf599151c033f3132e43153a1c1
momaki9 <mostafa_m9@yahoo.com>
2023-10-04 21:01:25 -0700

    installed react router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import Home from "./homePage/Home";
+import Test from "./fifthGradeMath/Test";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <Home />
+    <Router>
+    <Routes>
+      <Route 
+      path="/"
+      element={<Home />}
+       />
+      <Route 
+      path="/test"
+      element={<Test />}
+      />
+    </Routes>
+    </Router>
   );
 }
 
--- src/homePage/Home.js
+++ src/homePage/Home.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 function Home() {
     return (
         <>
@@ -5,6 +6,7 @@ function Home() {
             <h1>Some tips: Enter answers as is without any space before or after. For decimals ending in 0 (i.e. 2.40), omit the trailing 0.</h1>
             <a href="./fifthGradeMath/index.html"><button>Grade 5 Math</button></a>
             <a href="./seventhGradeMath/rates/index.html"><button>Grade 7 Math</button></a>
+            <Link to={"/test"} >Link</Link>
         </>
     )
 };
