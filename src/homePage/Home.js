91e6a0e86e290b74357ff3bea12cee6eb0535c84
momaki9 <mostafa_m9@yahoo.com>
2023-10-04 18:10:30 -0700

    refactored home page

---
+++ src/homePage/Home.js
@@ -0,0 +1,12 @@
+function Home() {
+    return (
+        <>
+            <h1>Welcome To Math Practice</h1>
+            <h1>Some tips: Enter answers as is without any space before or after. For decimals ending in 0 (i.e. 2.40), omit the trailing 0.</h1>
+            <a href="./fifthGradeMath/index.html"><button>Grade 5 Math</button></a>
+            <a href="./seventhGradeMath/rates/index.html"><button>Grade 7 Math</button></a>
+        </>
+    )
+};
+
+export default Home;
