e90b3284d4b159e0a86604f13720519970bf0d25
momaki9 <mostafa_m9@yahoo.com>
2023-10-06 20:57:36 -0700

    installed bootstrap

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import NavBar from "./components/NavBar";
 import Home from "./homePage/Home";
 import Test from "./fifthGradeMath/Test";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -5,16 +6,17 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <Router>
-    <Routes>
-      <Route 
-      path="/"
-      element={<Home />}
-       />
-      <Route 
-      path="/test"
-      element={<Test />}
-      />
-    </Routes>
+      <NavBar />
+      <Routes>
+        <Route
+          path="/"
+          element={<Home />}
+        />
+        <Route
+          path="/test"
+          element={<Test />}
+        />
+      </Routes>
     </Router>
   );
 }
--- src/homePage/Home.js
+++ src/homePage/Home.js
@@ -4,8 +4,9 @@ function Home() {
         <>
             <h1>Welcome To Math Practice</h1>
             <h1>Some tips: Enter answers as is without any space before or after. For decimals ending in 0 (i.e. 2.40), omit the trailing 0.</h1>
-            <a href="./fifthGradeMath/index.html"><button>Grade 5 Math</button></a>
-            <a href="./seventhGradeMath/rates/index.html"><button>Grade 7 Math</button></a>
+            <a href="../fifthGradeMath/index.html"><button>Grade 5 Math</button></a>
+            <a href="#"><button>Grade 7 Math</button></a>
+            <Link to={"./src/fifthGradeMath/index.html"}>ok</Link>
             <Link to={"/test"} >Link</Link>
         </>
     )
